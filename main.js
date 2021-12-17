f6985a41baa7b02b73004f7787ebb29f26a8e2bc
liliia kobzar <liliiakobzar2209@gmail.com>
2021-12-17 04:06:17 +0200

    added fourth homework

---
+++ main.js
@@ -0,0 +1,57 @@
+let tasks = [];
+let value = '';
+
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+let deleteBtns = [];
+
+let ul = document.createElement('ul');
+
+function deleteItem() {
+    if (deleteBtns.length) {
+        [...deleteBtns].map((btn) => {
+            btn.addEventListener('click', () => {
+                tasks.splice(btn.dataset.id, 1);
+                render();
+            })
+        })
+    }
+}
+
+function render() {
+    let str = '';
+
+    tasks.map((task, index) => {
+        str += `
+            <li>
+                <span>${task}</span>
+                <button data-id="${index}" class="delete-btn">X</button>
+            </li>
+        `;
+    });
+
+    ul.innerHTML = str;
+    document.body.prepend(ul);
+
+    deleteBtns = document.querySelectorAll('.delete-btn');
+    deleteItem();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    render();
+});
+
+input.addEventListener('input', (e) => { 
+    value = e.target.value;
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (value.trim()) {
+        tasks.push(value);
+    }
+    value = '';
+    form.reset();
+
+    render();
+});
